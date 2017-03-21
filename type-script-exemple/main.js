9a3062fe0eacc0b5e6ab7e55963247cf04921d84
Ygor Santos <ygor.gsan@gmail.com>
2017-03-21 11:51:38 -0300

    TypeScript exemple

---
+++ type-script-exemple/main.js
@@ -0,0 +1,23 @@
+var minhaVar = "Minha Variavel!";
+function minhaFunc(x, y) {
+    return x + y;
+}
+//ES 6 ou ES 2015
+var num = 2;
+var PI = 3.14;
+//
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; });
+var Matematica = (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+})();
+var n1 = 'string';
+n1 = 1;
