9b4dd4069d16687d43b4470b2122db6c888a69b0
Ygor Santos <ygor.gsan@gmail.com>
2017-03-24 11:27:46 -0300

    directives with property biding

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { OperadorElvisComponent } from './operador-elvis/operador-elvis.componen
 import { ExemploNgcontentComponent } from './exemplo-ngcontent/exemplo-ngcontent.component';
 import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
 import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
+import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
+import { HighlightDirective } from './shared/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretiv
     OperadorElvisComponent,
     ExemploNgcontentComponent,
     FundoAmareloDirective,
-    DiretivasCustomizadasComponent
+    DiretivasCustomizadasComponent,
+    HighlightMouseDirective,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
