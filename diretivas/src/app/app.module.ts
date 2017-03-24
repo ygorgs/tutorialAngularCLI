0fcbb8cdef75af88a59b7bd7e1fa607193f076ef
Ygor Santos <ygor.gsan@gmail.com>
2017-03-24 10:04:05 -0300

    ng-content

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.componen
 import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
 import { DiretivaNgsyleComponent } from './diretiva-ngsyle/diretiva-ngsyle.component';
 import { OperadorElvisComponent } from './operador-elvis/operador-elvis.component';
+import { ExemploNgcontentComponent } from './exemplo-ngcontent/exemplo-ngcontent.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { OperadorElvisComponent } from './operador-elvis/operador-elvis.componen
     DiretivaNgforComponent,
     DiretivaNgclassComponent,
     DiretivaNgsyleComponent,
-    OperadorElvisComponent
+    OperadorElvisComponent,
+    ExemploNgcontentComponent
   ],
   imports: [
     BrowserModule,
---
+++ diretivas/src/app/exemplo-ngcontent/exemplo-ngcontent.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { ExemploNgcontentComponent } from './exemplo-ngcontent.component';
+
+describe('ExemploNgcontentComponent', () => {
+  let component: ExemploNgcontentComponent;
+  let fixture: ComponentFixture<ExemploNgcontentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ExemploNgcontentComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExemploNgcontentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
