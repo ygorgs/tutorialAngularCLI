08bbd98f72dee12a0b8aa03e26b05a48fc4ccc82
Ygor Santos <ygor.gsan@gmail.com>
2017-03-24 14:29:37 -0300

    structure directive

--- diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
+++ diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivasCustomizadasComponent implements OnInit {
 
+  mostrarCursos: boolean = false;
+
+  onMostrarCursos(){
+    this.mostrarCursos = !this.mostrarCursos;
+  }
+
   constructor() { }
 
   ngOnInit() {
